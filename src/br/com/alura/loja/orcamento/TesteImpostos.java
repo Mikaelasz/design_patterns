5255faaf9d1886103890916fbf33b147767e563d
Mikaela Soares <miih.arj@gmail.com>
2021-06-19 19:54:26 -0300

    Pattern Chain of Responsibility

--- src/br/com/alura/loja/orcamento/TesteImpostos.java
+++ src/br/com/alura/loja/orcamento/TesteImpostos.java
@@ -1,15 +1,14 @@
-package br.com.alura.loja;
+package br.com.alura.loja.orcamento;
 
 import java.math.BigDecimal;
 
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
 import br.com.alura.loja.imposto.ISS;
-import br.com.alura.loja.orcamento.Orcamento;
 
 public class TesteImpostos {
 
 	public static void main(String[] args) {
-		Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+		Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
 		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
 		System.out.println(calculadora.calcular(orcamento, new ISS()));
 	}
